47eebcfdcb742226d242cda39331c4efa7a7c620
Kelly Kearney <kakearney@gmail.com>
2016-06-29 11:31:21 -0400

    Began combinegroups method

---
+++ @ecopathmodel/combinegroups.m
@@ -0,0 +1,28 @@
+function combinegroups(EM, grps)
+
+% Calculate new-group index array
+
+grps = grps(:);
+gidx = (1:length(grps))';
+
+idx = cellfun(@(x,n) ones(size(x))*n, grps, num2cell(gidx), 'uni', 0);
+idx = cat(1, idx{:});
+
+[tf, loc] = ismember(EM.name, cat(1, grps{:}));
+
+aidx = nan(EM.ngroup,1);
+aidx(tf) = idx(loc(tf));
+
+aidx(~tf) = (1:sum(~tf)) + max(gidx);
+
+% Aggregate biomass (use balanced version)
+
+Ep = EM.ecopath;
+
+[~, b] = aggregate(aidx, Ep.b);
+
+bsum = 
+
+
+
+blah
