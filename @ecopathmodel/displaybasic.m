01366580e97c112167a018aa32946ff5906a8a2e
Kelly Kearney <kakearney@gmail.com>
2016-07-12 13:59:03 -0700

    Added cflag option to displaybasic method
    Added an option to display Ecopath basic table without the cprintf
    color options.  This should be used when printing somewhere other
    than the Matlab Command Window (-nodesktop mode, publishing, etc)

--- @ecopathmodel/displaybasic.m
+++ @ecopathmodel/displaybasic.m
@@ -1,8 +1,9 @@
-function displaybasic(In, Ep)
+function displaybasic(In, Ep, cflag)
 %DISPLAYBASIC Prints ecopath results to screen
 %
 % displayecopath(EM)
 % displayecopath(EM, Ep)
+% displayecopath(EM, Ep, cflag)
 %
 % This function displays main parameters of an ecopathmodel in the
 % command window, using a table structure similar to the way Ecopath
@@ -17,12 +18,24 @@ function displaybasic(In, Ep)
 %           be displayed as empty. If included, the calculated values are
 %           shown in blue; values indicative of an unbalanced model are
 %           shown in red.
+%
+%   cflag:  flag to print using color (via cprintf function).  The
+%           cprintf function uses tags specifically formatted for the
+%           Matlab command window; if printing to a different environment
+%           (e.g., publishing a document, running in nodesktop mode), you
+%           may want to turn this off.  Default is true.
 
 % Copyright 2016 Kelly Kearney
 
 % Parse input
 
-epflag = nargin > 1;
+narginchk(1,3);
+
+epflag = nargin > 1 && ~isempty(Ep);
+if nargin < 3
+    cflag = true;
+end
+
 
 % Display basic input/estimates (TL HA BH B PB QB EE GE GS DI)
 
@@ -73,26 +86,47 @@ for ic = 1:ncol
 end
 [dispstr{strcmp(dispstr, 'NaN')}] = deal(' ');
 
-fmt = sprintf('%%-%ds ', 3, gwidth, wdth);
-cprintf('-text', fmt, '   ', 'Name', 'TL', 'HA', 'BH', 'B', 'PB', 'QB', 'EE', 'GE', 'GS', 'DI');
-fprintf('\n');
-
-for ir = 1:nrow
-    fmt = sprintf('%%2d: %%-%ds', gwidth+1);
-    fprintf(fmt, ir, In.name{ir});
-    for ic = 1:ncol
-        fmt = sprintf('%%%ds', wdth(ic)+1);
-        if isoutofrange(ir,ic)
-            cprintf('red', fmt, dispstr{ir,ic});
-        elseif isfilled(ir,ic)
-            cprintf('blue', fmt, dispstr{ir,ic});
-        else
-            cprintf('text',fmt, dispstr{ir,ic});
-        end
-    end
+if cflag
+    fmt = sprintf('%%-%ds ', 3, gwidth, wdth);
+    cprintf('-text', fmt, '   ', 'Name', 'TL', 'HA', 'BH', 'B', 'PB', 'QB', 'EE', 'GE', 'GS', 'DI');
     fprintf('\n');
-end
 
+    for ir = 1:nrow
+        fmt = sprintf('%%2d: %%-%ds', gwidth+1);
+        fprintf(fmt, ir, In.name{ir});
+        for ic = 1:ncol
+            fmt = sprintf('%%%ds', wdth(ic)+1);
+            if isoutofrange(ir,ic)
+                cprintf('red', fmt, dispstr{ir,ic});
+            elseif isfilled(ir,ic)
+                cprintf('blue', fmt, dispstr{ir,ic});
+            else
+                cprintf('text',fmt, dispstr{ir,ic});
+            end
+        end
+        fprintf('\n');
+    end
+else
+    fmt = sprintf('%%-%ds ', 3, gwidth, wdth);
+    fprintf(fmt, '   ', 'Name', 'TL', 'HA', 'BH', 'B', 'PB', 'QB', 'EE', 'GE', 'GS', 'DI');
+    fprintf('\n');
+
+    for ir = 1:nrow
+        fmt = sprintf('%%2d: %%-%ds', gwidth+1);
+        fprintf(fmt, ir, In.name{ir});
+        for ic = 1:ncol
+            fmt = sprintf('%%%ds', wdth(ic)+1);
+            if isoutofrange(ir,ic)
+                fprintf(fmt, dispstr{ir,ic});
+            elseif isfilled(ir,ic)
+                fprintf(fmt, dispstr{ir,ic});
+            else
+                fprintf(fmt, dispstr{ir,ic});
+            end
+        end
+        fprintf('\n');
+    end
+end
 
            
            
